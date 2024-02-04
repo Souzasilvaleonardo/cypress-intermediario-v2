46f4073c248e694b6cd0c68ee6d09322dc0a69c7
Souzasilvaleonardo <souzasilva.leonardo19@gmail.com>
2024-02-03 22:18:18 -0300

    novo teste para Issue via API

--- cypress/support/api_commands.js
+++ cypress/support/api_commands.js
@@ -30,3 +30,19 @@ Cypress.Commands.add('api_deleteProjects', function() {
     }))
   )
 })
+
+Cypress.Commands.add('api_createIssue', function(issue) {
+  cy.api_createProject(issue.project)
+    .then (response => {
+      cy.request({
+        method: 'POST',
+        url: `/api/v4/projects/${response.body.id}/issues`,
+        body: {
+          title: issue.title,
+          description: issue.description
+        },
+        headers: { Authorization: accessToken },
+      })
+    })
+
+})
